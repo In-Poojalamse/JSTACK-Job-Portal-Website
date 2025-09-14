a192e557d15048c5d0158edea51b1ab066fbeb4d
Poojalamse <poojalamse05@gmail.com>
2025-09-14 17:29:16 +0530

    Initial Render setup with PHPMailer and NGINX

--- PHPMailer/send_email.php
+++ PHPMailer/send_email.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $name    = $_POST['name'];
